f2abfab706cee8791984a773857fb9f5a26b38d1
Anderson Rodrigues <anddersonrds@gmail.com>
2020-08-04 22:05:12 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-   <h1>Clone Github</h1>
+   <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<Profile />} />
+      <Route path="/:username" element={<Profile />} />
+      <Route path="/:username/:repo" element={<Repo />} />
+    </Routes>
+
+    {/* <Footer /> */}
+    <GlobalStyles />
+   </BrowserRouter>
   );
 }
 
